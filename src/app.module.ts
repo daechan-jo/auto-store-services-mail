c991fd63689db802297700f3fa443f2051235f6b
JoDaeChan <daechan476@gmail.com>
2025-04-17 21:19:43 +0900

    refactor: update environment configuration and enhance application bootstrap process

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
@@ -6,13 +5,16 @@ import { MailController } from './api/mail.controller';
 import { MailService } from './core/mail.service';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			envFilePath: '/Users/daechanjo/codes/project/auto-store/.env',
-		}),
-	],
-	controllers: [MailController],
-	providers: [MailService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV !== 'PROD'
+          ? '/Users/daechanjo/codes/project/auto-store/.env'
+          : '/app/.env',
+    }),
+  ],
+  controllers: [MailController],
+  providers: [MailService],
 })
 export class AppModule {}
